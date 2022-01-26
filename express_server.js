4e773868aedfc41709cba6d44fde5bedc504d7c6
Jiani Cao <jianicakes@gmail.com>
2022-01-25 20:31:29 -0800

    add route for login

--- express_server.js
+++ express_server.js
@@ -70,6 +70,12 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/login", (req, res) => {
+  console.log(req.body.username);
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
