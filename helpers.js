faa805601b7b6852cc6c4aefe65591679f5eb2ee
Jiani Cao <jianicakes@gmail.com>
2022-01-27 23:27:15 -0800

    add & classify helper functions

--- helpers.js
+++ helpers.js
@@ -1,4 +1,4 @@
-  //// generate random id ////
+//// generate random id ////
 function generateRandomString() {
   return Math.random().toString(36).substring(3, 9);
 }
@@ -25,8 +25,80 @@ const urlsForUser = (id, database) => {
   return userURLs;
 };
 
+const generateUserHelpers = (users, bcrypt) => {
+
+//// return error if user email/password are not a match ////
+const authenticateUser = (user) => {
+  const { email, password } = user;
+  const foundUser = findUserByEmail(email, users);
+
+  if (!foundUser) {
+    return { error: "Email doesn't exist.", data: null };
+  }
+
+  if (!bcrypt.compareSync(password, foundUser.password)) {
+    return { error: "Password doesn't match.", data: null };
+  }
+
+  return { error: null, data: foundUser };
+};
+
+//// add a new user, return error if criteria not met ////
+const addUser = (user) => {
+  const { email, password } = user;
+
+  if (!email || !password) {
+    return { error: "Email and password cannot be blank!", data: null };
+  }
+
+  const foundUser = findUserByEmail(email, users);
+  if (foundUser) {
+    return { error: "An user with that email already exists!", data: null };
+  }
+
+  const userID = generateRandomString();
+  const hashedPassword = bcrypt.hashSync(password);
+
+  const newUser = {id: userID, email, password: hashedPassword};
+  users[userID] = newUser;
+
+  return { error: null, data: newUser };
+};
+
+  return { authenticateUser, addUser };
+}
+
+const generateUrlHelpers = (urlDatabase) => {
+
+  //// determine if URL can be modified ////
+  const modifyURL = (url, id) => {
+    const { shortURL } = url;
+    const { userID } = id;
+
+    if (!urlDatabase[shortURL]) {
+      return { error: "URL not found!", urlID: null };
+    }
+
+    if (!userID) {
+      return { error: "You must be logged in to make changes to an URL!", urlID: null };
+    }
+
+    const userURLs = urlsForUser(userID, urlDatabase);
+    if (!userURLs[shortURL]) {
+      return { error: "You do not have permission to make changes to this URL!", urlID: null };
+    }
+
+    return { error: null, urlID: shortURL };
+  };
+
+  return { modifyURL };
+};
+
+
 module.exports = {
   generateRandomString,
   findUserByEmail,
-  urlsForUser
+  urlsForUser,
+  generateUserHelpers,
+  generateUrlHelpers
 };
